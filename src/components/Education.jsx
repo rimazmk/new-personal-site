aca3cf6e69329f14adc079eaf543812b8b5e2a87
Rimaz Khan <rimazk@umich.edu>
2020-12-25 15:50:12 -0500

    add education

---
+++ src/components/Education.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../css/Education.scss";
+import data from "../data.json";
+import logo from "../images/U-M-logo.jpg";
+
+const Education = () => {
+  let l = data.education[0].courses.length;
+  let h1 = data.education[0].courses.slice(0, l / 2);
+  let h2 = data.education[0].courses.slice(l / 2);
+
+  const displayCourses = (courses) => {
+    return courses.map((course) => {
+      return <p className="course-name">{course}</p>;
+    });
+  };
+
+  return (
+    <>
+      <h1>Education 🎓</h1>
+      <div className="card-horizontal">
+        <div className="card-horizontal-text">
+          <h2>University of Michigan</h2>
+          <p>B.S.E. Computer Science</p>
+          <p>GPA: 3.61/4.0</p>
+          <p>Sep. 2018 - Dec. 2021</p>
+        </div>
+        <img src={logo} className="card-horizontal-img" />
+        <h2 className="courses-title">Courses</h2>
+        <div className="courses-outer-container">
+          <div className="courses-inner-container">{displayCourses(h1)}</div>
+          <div className="courses-inner-container">{displayCourses(h2)}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Education;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,14 +4,18 @@ import Particles from "react-particles-js";
 import AboutCard from "./AboutCard";
 import AboutMe from "./AboutMe";
 import CustomNavbar from "./CustomNavbar";
+import Education from "./Education";
 
 const LandingPage = () => {
   return (
     <>
       <Particles className="landing-page-background" canvasClassName="canvas" />
-      <AboutCard />
-      <AboutMe />
       <CustomNavbar />
+      <AboutCard />
+      <div className="container-main">
+        <AboutMe />
+        <Education />
+      </div>
     </>
   );
 };
